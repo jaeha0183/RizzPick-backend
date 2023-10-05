710e5f1c49ac39ce25a3158e4c89fc7c5c6fed27
jwywoo <jwywoo26@gmail.com>
2023-10-05 11:13:25 +0900

    init commit

---
+++ src/main/java/com/willyoubackend/WillYouBackendApplication.java
@@ -0,0 +1,13 @@
+package com.willyoubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WillYouBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WillYouBackendApplication.class, args);
+    }
+
+}
