08624e909df0f7e8d34686189137b62297f76cfb
DESKTOP-K1PBAQ0\win10 <kwe4248@gmail.com>
2023-10-05 12:15:32 +0900

    Feat: "로그인 및 회원가입 구현"
    로그인 및 회원가입 API 개발
    
    Resolves: #1
    Ref: None
    Related to: None

---
+++ src/main/java/com/willyoubackend/domain/user/dto/UserInfoDto.java
@@ -0,0 +1,10 @@
+package com.willyoubackend.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserInfoDto {
+    String username;
+}
---
+++ src/main/java/com/willyoubackend/domain/user/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.willyoubackend.domain.user.repository;
+
+import com.willyoubackend.domain.user.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
+
+    Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findById(Long id);
+
+}
