fe4280af3733fcfe5a489aff2ef2207d534e0756
DESKTOP-K1PBAQ0\win10 <kwe4248@gmail.com>
2023-10-12 21:00:14 +0900

    [#42] Add: "Websocket-Chat"
    Websocket Security적용 및 Redis DB연결
    
    Resolves: #42
    Ref: None
    Related to: #42

--- src/main/java/com/willyoubackend/domain/websocket/config/WebSockConfig.java
+++ src/main/java/com/willyoubackend/domain/websocket/config/WebSockConfig.java
@@ -1,14 +1,18 @@
 package com.willyoubackend.domain.websocket.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompHandler stompHandler;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -21,5 +25,10 @@ public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws-stomp")
                 .setAllowedOriginPatterns("http://localhost:8080")
                 .withSockJS();
+    }
+
+    @Override
+    public void configureClientInboundChannel(org.springframework.messaging.simp.config.ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 }
