67ab6c3dc411971defcda667d330a1993419d706
DESKTOP-K1PBAQ0\win10 <kwe4248@gmail.com>
2023-10-18 18:12:19 +0900

    [#108] Add: "Chat API"
    매칭 시 채팅방 생성 기능 구현 및 내가 속한 채팅방 목록 조회
    
    Resolves: #108
    Ref: #108
    Related to: #108

--- src/main/java/com/willyoubackend/domain/websocket/entity/ChatRoomRequestDto.java
+++ src/main/java/com/willyoubackend/domain/websocket/entity/ChatRoomRequestDto.java
@@ -1,8 +1,10 @@
 package com.willyoubackend.domain.websocket.entity;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class ChatRoomRequestDto {
     private Long chatRoomId;
 
