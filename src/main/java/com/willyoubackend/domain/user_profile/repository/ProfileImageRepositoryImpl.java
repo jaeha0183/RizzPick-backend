06bf2cff1a71b253e4d2b74d734f1e6d1cd33994
JJW11111 <zizrar2@gmail.com>
2023-10-26 14:03:59 +0900

    [#147]Feat: "QueryDSL NPE"
    QueryDSL NPE 대응
    
    Resolves: #147
    Ref: None
    Related to: None

--- src/main/java/com/willyoubackend/domain/user_profile/repository/ProfileImageRepositoryImpl.java
+++ src/main/java/com/willyoubackend/domain/user_profile/repository/ProfileImageRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.willyoubackend.domain.user_profile.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.willyoubackend.domain.user.entity.UserEntity;
 import com.willyoubackend.domain.user_profile.entity.ProfileImageEntity;
@@ -14,14 +15,16 @@ import java.util.List;
 public class ProfileImageRepositoryImpl implements ProfileImageRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
+    private final QProfileImageEntity profileImage = QProfileImageEntity.profileImageEntity;
 
     @Override
-    public List<ProfileImageEntity> findAllByUserEntity(UserEntity userEntity) {
-
-        QProfileImageEntity profileImageEntity = QProfileImageEntity.profileImageEntity;
-
-        return jpaQueryFactory.selectFrom(profileImageEntity)
-                .where(profileImageEntity.userEntity.eq(userEntity))
+    public List<ProfileImageEntity> findAllByUserEntity(UserEntity user) {
+        return jpaQueryFactory.selectFrom(profileImage)
+                .where(userEntityEq(user))
                 .fetch();
+    }
+
+    private BooleanExpression userEntityEq(UserEntity user) {
+        return user != null ? profileImage.userEntity.eq(user) : null;
     }
 }
--- src/main/java/com/willyoubackend/domain/user_profile/repository/UserProfileRepositoryImpl.java
+++ src/main/java/com/willyoubackend/domain/user_profile/repository/UserProfileRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.willyoubackend.domain.user_profile.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.willyoubackend.domain.user.entity.UserEntity;
 import com.willyoubackend.domain.user_profile.entity.QUserProfileEntity;
@@ -12,15 +13,18 @@ import org.springframework.stereotype.Repository;
 public class UserProfileRepositoryImpl implements UserProfileRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
+    private final QUserProfileEntity userProfile = QUserProfileEntity.userProfileEntity;
+
 
     @Override
-    public UserProfileEntity findByUserEntity(UserEntity userEntity) {
-
-        QUserProfileEntity userProfileEntity = QUserProfileEntity.userProfileEntity;
-
-        return jpaQueryFactory.select(userProfileEntity)
-                .from(userProfileEntity)
-                .where(userProfileEntity.userEntity.eq(userEntity))
+    public UserProfileEntity findByUserEntity(UserEntity user) {
+        return jpaQueryFactory.select(userProfile)
+                .from(userProfile)
+                .where(userEntityEq(user))
                 .fetchOne();
+    }
+
+    private BooleanExpression userEntityEq(UserEntity user) {
+        return user != null ? userProfile.userEntity.eq(user) : null;
     }
 }
