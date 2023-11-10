489e949c41deb5cbdf2602967d291de9b5a33daa
JJW11111 <zizrar2@gmail.com>
2023-11-10 11:32:45 +0900

    [#236]Fix: "SSE Image"
    SSE Image
    
    Resolves: #236
    Ref: None
    Related to: None

--- src/main/java/com/willyoubackend/domain/sse/dto/UserDto.java
+++ src/main/java/com/willyoubackend/domain/sse/dto/UserDto.java
@@ -1,15 +1,21 @@
 package com.willyoubackend.domain.sse.dto;
 
 import com.willyoubackend.domain.user.entity.UserEntity;
+import com.willyoubackend.domain.user_profile.dto.ImageResponseDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
 public class UserDto {
     private Long id;
+    private List<ImageResponseDto> profileImages;
 
     public UserDto(UserEntity user) {
         this.id = user.getId();
+        this.profileImages = user.getProfileImages().stream().map(ImageResponseDto::new).collect(Collectors.toList());
     }
 }
