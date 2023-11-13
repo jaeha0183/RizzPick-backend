f6aceff0b392bb74a0519ed527c3aa679959d7cc
Jaeha Lee <jaeha0183@gmail.com>
2023-11-13 23:03:13 +0900

    [#132] fix: "chatroom controller userDetails"
    @AuthenticationPrinciple 적용
    
    Resolves: #132
    Ref: None
    Related to: None

--- src/main/java/com/willyoubackend/domain/websocket/controller/ChatRoomController.java
+++ src/main/java/com/willyoubackend/domain/websocket/controller/ChatRoomController.java
@@ -6,6 +6,7 @@ import com.willyoubackend.domain.websocket.entity.ChatRoomInfoDto;
 import com.willyoubackend.domain.websocket.service.ChatRoomService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,16 +24,14 @@ public class ChatRoomController {
 
     @Operation(summary = "내가 속한 체팅방 목록 조회")
     @GetMapping("/rooms/me")
-    public List<ChatRoomDto> getMyChatRooms() {
-        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    public List<ChatRoomDto> getMyChatRooms(@AuthenticationPrincipal UserDetailsImpl userDetails) {
 
         return chatRoomService.findChatRoomsByUserId(userDetails.getUser().getId());
     }
 
     @Operation(summary = "특정 채팅방 정보 조회")
     @GetMapping("/rooms/{chatRoomId}")
-    public ChatRoomInfoDto getChatRoomInfo(@PathVariable Long chatRoomId) {
-        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    public ChatRoomInfoDto getChatRoomInfo(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long chatRoomId) {
 
         return chatRoomService.findChatRoomInfo(chatRoomId, userDetails.getUser().getId());
     }
